e781fa824cfcdbd177a407a1329c3a4a2b920826
JeremYD <delpech.jeremy@hotmail.fr>
2020-11-13 16:22:58 +0100

    New Spring Boot project

---
+++ src/test/java/com/jeremd/keyboardshortcutsmemo/KeyboardShortcutsMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremd.keyboardshortcutsmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyboardShortcutsMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
